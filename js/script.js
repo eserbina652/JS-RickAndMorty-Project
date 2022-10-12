ed7b45429b9b74efbee7ea08e1a33a11067bf7f8
Yevheniia <eserbina652@gmail.com>
2022-10-12 19:12:06 +0200

    Js Project

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const form = document.getElementById('form')
+// const table = document.getElementById('content')
+const wrapper = document.querySelector('#content')
+const table = document.createElement('table')
+const headerText = document.createElement('div')
+const tBody = document.createElement('tbody')
+const trString = document.createElement('tr')
+
+const tdName = document.createElement('td')
+const tdColor = document.createElement('td')
+const tdDiagonal = document.createElement('td')
+
+
+headerText.innerText = 'Parameters of the desired device'
+tdName.innerText = 'Name'
+tdColor.innerText = 'Color'
+tdDiagonal.innerText = 'Diagonal'
+headerText.style.whiteSpace = 'nowrap'
+headerText.style.backgroundColor = 'rgb(63, 77, 61)'
+headerText.style.border = '2px solid #5ab055'
+headerText.style.fontSize = '25px'
+headerText.style.padding = '20px 0 20px 0'
+wrapper.appendChild(headerText)
+
+trString.appendChild(tdName)
+trString.appendChild(tdColor)
+trString.appendChild(tdDiagonal)
+
+tBody.appendChild(trString)
+table.appendChild(tBody)
+wrapper.appendChild(table)
+
+
+function retriveFormValue(event) {
+  event.preventDefault()
+  const name = form.querySelector('[name="name"]')
+  const color = form.querySelector('[name="color"]')
+  const diagonal = form.querySelector('[name="diagonal"]')
+
+  // const values = {
+  //   name: name.value,
+  //   color: color.value,
+  //   diagonal: diagonal.value
+  // }
+  const trBody = document.createElement('tr')
+  const tdName = document.createElement('td')
+  const tdColor = document.createElement('td')
+  const tdDiagonal = document.createElement('td')
+
+
+  // thName.style.width = '250px'
+  // thName.style.marginRight = '5px'
+  // thName.style.border = '1px solid #5ab055'
+  // thName.style.backgroundColor = 'rgb(63, 77, 61)'
+  // thName.style.fontSize = '20px'
+  // thName.style.padding = '20px'
+
+  tdName.innerText = name.value
+  tdColor.innerText = color.value
+  tdDiagonal.innerText = diagonal.value
+
+  trBody.appendChild(tdName)
+  trBody.appendChild(tdColor)
+  trBody.appendChild(tdDiagonal)
+  table.appendChild(trBody)
+}
+
+form.addEventListener('submit', retriveFormValue)
+
+
---
+++ js/server.js
@@ -0,0 +1,136 @@
+const popupLinks = document.querySelectorAll('.popup-link')
+const body = document.querySelector('body')
+const lockPadding = document.querySelectorAll('.lock-padding')
+
+let unlock = true
+
+const timeout = 800
+
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupLinks.length; index++) {
+    const popupLink = popupLinks[index]
+    popupLink.addEventListener('click', function (e) {
+      const popupName = popupLink.getAttribute('href').replace('#', '')
+      const currentPopup = document.getElementById(popupName)
+      popupOpen(currentPopup)
+      e.preventDefault()//если событие не выполняется явно, его действие по умолчанию не должно выполняться так, как обычно
+    })
+  }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup')
+if (popupCloseIcon.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++) {
+    const el = popupCloseIcon[index]
+    el.addEventListener('click', function (e) {
+      popupClose(el.closest('popup'))
+      e.preventDefault()
+    })
+  }
+}
+
+function popupOpen(currentPopup) {
+  if (currentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open')
+    if (popupActive) {
+      popupClose(popupActive, false)
+    } else {
+      bodyLock()
+    }
+    currentPopup.classList.add('open')
+    currentPopup.addEventListener('click', function (e) {
+      if (!e.target.closest('popup_content')) {
+        popupClose(e.target.closest('.popup'))
+      }
+    })
+  }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove('open')
+  } if (doUnlock) {
+    bodyUnLock()
+  }
+}
+
+function bodyLock() {
+  const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
+  if (lockPadding.length > 0)
+    for (let index = 0; index < lockPadding.length; index++) {
+      const el = lockPadding[index]
+      el.style.paddingRight = lockPaddingValue
+    }
+  body.style.paddingRight = lockPaddingValue
+  body.classList.add('lock')
+
+  unlock = false
+  setTimeout(function () {
+    unlock = true
+  }, timeout)
+}
+
+function bodyUnLock() {
+  setTimeout(function () {
+    if (lockPadding.length > 0) {
+      for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index]
+        el.style.paddingRight = '0px'
+      }
+    }
+    body.style.paddingRight = '0px'
+    body.classList.remove('lock')
+  }, timeout)
+
+  unlock = false
+  setTimeout(function () {
+    unlock = true
+  }, timeout)
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.which === 27) {
+    const  popupActive = document.querySelector('.popup.open')
+    popupClose(popupActive)
+  }
+})
+
+const div = document.getElementById('APIBlock')
+div.style.width = 'auto'
+// div.style.display = 'flex'
+// div.style.flexDirection = 'row'
+// div.style.flexWrap = 'wrap'
+// div.style.alignItems = 'flex-start'
+// div.style.justifyContent = 'space around'
+// div.style.alignContent = 'flex-start'
+// const button = document.getElementById('button')
+// button.style.height = '70px'
+// button.style.width = '70px'
+// button.style.backgroundColor = 'red'
+// button.style.borderRadius = '20px'
+// button.style.marginLeft = '450px'
+// button.style.marginTop = '30px'
+
+
+async function getResponse(name) {
+  let response = await fetch(`https://rickandmortyapi.com/api/character`)
+  let obj = await response.json()
+  obj = obj.results
+  obj.forEach(el => {
+    let img = document.createElement('img')
+    let p = document.createElement('p')
+    p.innerText = el.name
+    p.style.fontSize = '40px'
+    p.style.color = '#5dff55'
+    img.style.width = 'auto'
+    img.style.height = 'auto'
+    img.style.border = '3px solid green'
+    img.style.marginTop = '30px'
+    img.src = el.image
+    div.appendChild(img)
+    img.append(p)
+    div.appendChild(p)
+  })
+}
+
+getResponse()
