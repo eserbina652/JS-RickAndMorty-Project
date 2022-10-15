f831312d160cbe3bcf39c9a20f78af72d29ec41b
Yevheniia <eserbina652@gmail.com>
2022-10-15 15:32:10 +0200

    changeLocalStorage

--- js/server.js
+++ js/server.js
@@ -1,7 +1,6 @@
 const popupLinks = document.querySelectorAll('.popup-link')
 const body = document.querySelector('body')
 const lockPadding = document.querySelectorAll('.lock-padding')
-const neForm = document.getElementById('neForm')
 
 let unlock = true
 
@@ -14,7 +13,7 @@ if (popupLinks.length > 0) {
       const popupName = popupLink.getAttribute('href').replace('#', '')
       const currentPopup = document.getElementById(popupName)
       popupOpen(currentPopup)
-      e.preventDefault()//если событие не выполняется явно, его действие по умолчанию не должно выполняться так, как обычно
+      e.preventDefault()
     })
   }
 }
@@ -39,13 +38,19 @@ function popupOpen(currentPopup) {
       bodyLock()
     }
     currentPopup.classList.add('open')
+    currentPopup.addEventListener('click', function (e) {
+      if (!e.target.closest('popup_content')) {
+        popupClose(e.target.closest('.popup'))
+      }
+    })
   }
 }
 
 function popupClose(popupActive, doUnlock = true) {
   if (unlock) {
     popupActive.classList.remove('open')
-  } if (doUnlock) {
+  }
+  if (doUnlock) {
     bodyUnLock()
   }
 }
@@ -86,30 +91,96 @@ function bodyUnLock() {
 
 document.addEventListener('keydown', function (e) {
   if (e.which === 27) {
-    const  popupActive = document.querySelector('.popup.open')
+    const popupActive = document.querySelector('.popup.open')
     popupClose(popupActive)
   }
 })
 
+
+const form = document.getElementById('form')
+const wrapper = document.querySelector('#content')
+const table = document.createElement('table')
+const headerText = document.createElement('div')
+const tBody = document.createElement('tbody')
+const trString = document.createElement('tr')
+
+const tdName = document.createElement('td')
+const tdColor = document.createElement('td')
+const tdDiagonal = document.createElement('td')
+
+
+headerText.innerText = 'Parameters of the desired device'
+tdName.innerText = 'Name'
+tdColor.innerText = 'Color'
+tdDiagonal.innerText = 'Diagonal'
+headerText.style.whiteSpace = 'nowrap'
+headerText.style.backgroundColor = 'rgb(63, 77, 61)'
+headerText.style.border = '2px solid #5ab055'
+headerText.style.fontSize = '25px'
+headerText.style.padding = '20px 0 20px 0'
+wrapper.appendChild(headerText)
+
+trString.appendChild(tdName)
+trString.appendChild(tdColor)
+trString.appendChild(tdDiagonal)
+
+tBody.appendChild(trString)
+table.appendChild(tBody)
+wrapper.appendChild(table)
+
+
+function retriveFormValue(event) {
+  event.preventDefault()
+  console.log(event)
+  const name = form.querySelector('[name="name"]')
+  const color = form.querySelector('[name="color"]')
+  const diagonal = form.querySelector('[name="diagonal"]')
+
+  const trBody = document.createElement('tr')
+  const tdName = document.createElement('td')
+  const tdColor = document.createElement('td')
+  const tdDiagonal = document.createElement('td')
+
+  // trBody.setAttribute('id', 'table_body')
+
+  let data = {}
+  const submit = document.getElementById('neForm')
+  submit.addEventListener('click', function (event) {
+    console.log(event.target.value)
+    data[event.target.name] = event.target.value
+    localStorage.setItem('formData', JSON.stringify(data))
+  })
+  if (localStorage.getItem('formData')) {
+    data = JSON.parse(localStorage.getItem('formData'))
+
+    for (let key in data) {
+      form.elements[key].value = data[key]
+    }
+  }
+
+
+  tdName.innerText = name.value
+  tdColor.innerText = color.value
+  tdDiagonal.innerText = diagonal.value
+
+  trBody.appendChild(tdName)
+  trBody.appendChild(tdColor)
+  trBody.appendChild(tdDiagonal)
+
+
+  table.appendChild(trBody)
+
+}
+
+
+form.addEventListener('submit', retriveFormValue)
+
+
 const div = document.getElementById('APIBlock')
 div.style.width = 'auto'
-// div.style.display = 'flex'
-// div.style.flexDirection = 'row'
-// div.style.flexWrap = 'wrap'
-// div.style.alignItems = 'flex-start'
-// div.style.justifyContent = 'space around'
-// div.style.alignContent = 'flex-start'
-// const button = document.getElementById('button')
-// button.style.height = '70px'
-// button.style.width = '70px'
-// button.style.backgroundColor = 'red'
-// button.style.borderRadius = '20px'
-// button.style.marginLeft = '450px'
-// button.style.marginTop = '30px'
 
-
-async function getResponse(name) {
-  let response = await fetch(`https://rickandmortyapi.com/api/character`)
+async function getResponse() {
+  let response = await fetch('https://rickandmortyapi.com/api/character')
   let obj = await response.json()
   obj = obj.results
   obj.forEach(el => {
@@ -120,8 +191,8 @@ async function getResponse(name) {
     p.style.color = '#5dff55'
     img.style.width = 'auto'
     img.style.height = 'auto'
-    img.style.border = '3px solid green'
-    img.style.marginTop = '30px'
+    img.style.border = '3px solid #5dff55'
+    img.style.marginTop = '70px'
     img.src = el.image
     div.appendChild(img)
     img.append(p)
@@ -130,8 +201,3 @@ async function getResponse(name) {
 }
 
 getResponse()
-neForm.addEventListener('click', function (e) {
-  if (!e.target.closest('popup_content')) {
-    popupClose(e.target.closest('.popup'))
-  }
-})
